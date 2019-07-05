b786a17cec2037fbfeefdc4146460c75f3bea6cb
REZARE\delangea <andrew.de.lange@rezare.co.nz>
2019-07-05 15:41:43 +1200

    First Commit for Repository

---
+++ mobile-examples/mobile-examples/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using mobile_examples.Models;
+
+namespace mobile_examples.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
