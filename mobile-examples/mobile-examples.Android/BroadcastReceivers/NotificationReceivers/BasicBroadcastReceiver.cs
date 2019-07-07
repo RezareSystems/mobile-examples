273502156ff976632893e209864d28ece5a58161
REZARE\delangea <andrew.de.lange@rezare.co.nz>
2019-07-08 09:01:47 +1200

    no message

---
+++ mobile-examples/mobile-examples.Android/BroadcastReceivers/NotificationReceivers/BasicBroadcastReceiver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace mobile_examples.Droid.BroadcastReceivers.NotificationReceivers
+{
+    [BroadcastReceiver(Enabled = true, Exported = false)]
+    [IntentFilter(new[] { "actionButton" })] // Should match the name of the intent from the button you wish to receive
+    public class BasicBroadcastReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent != null)
+            {
+                // Your handler goes here
+                var app = (App)Xamarin.Forms.Application.Current;
+            }
+        }
+    }
+}
--- mobile-examples/mobile-examples.Android/MainActivity.cs
+++ mobile-examples/mobile-examples.Android/MainActivity.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using mobile_examples.Droid.BroadcastReceivers.NotificationReceivers;
 
 namespace mobile_examples.Droid
 {
@@ -22,7 +24,12 @@ namespace mobile_examples.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+#region Notifications
+            RegisterReceiver(new BasicBroadcastReceiver(), new IntentFilter("actionButton"));
+#endregion
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
